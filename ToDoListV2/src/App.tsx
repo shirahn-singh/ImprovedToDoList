ca127dcbbe581a10a2e90d5bc6a2b8c2345f1160
Shirahn <shirahn@gendac.co.za>
2024-06-03 11:53:02 +0200

    Added To Do List item component implementation

--- ToDoListV2/src/App.tsx
+++ ToDoListV2/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import IndividualTask from "./Components/IndividualTask";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <IndividualTask
+        text="Hello There"
+        onCloseTask={() => console.log("Close Pressed")}
+      ></IndividualTask>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
